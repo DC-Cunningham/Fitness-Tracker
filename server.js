640f65f44e63c4d425e52e9c6b0a0307a8463dfa
DCRevResLabs <dc@revolutionresearch.com.au>
2020-06-06 22:11:36 +1000

    heroku db setup

--- server.js
+++ server.js
@@ -13,10 +13,13 @@ app.use(express.static("public"));
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.obscure - stream - 55645 || "mongodb://localhost/workout",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
